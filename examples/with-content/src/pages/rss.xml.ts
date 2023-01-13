ecd537dcbdf19f77ce0adf5ecdee82b9b51f01e4
bholmesdev <hey@bholmes.dev>
2023-01-13 10:37:45 -0500

    chore: clean up with-content rss example

--- examples/with-content/src/pages/rss.xml.ts
+++ examples/with-content/src/pages/rss.xml.ts
@@ -1,12 +1,13 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function get() {
+export async function get({ site }) {
 	const blog = await getCollection('blog');
 	return rss({
-		title: 'My Astro Site',
-		description: 'My Astro Site Description',
-		site: 'https://example.com',
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site,
 		items: blog.map((item) => ({ ...item.data, link: `/blog/${item.slug}/` })),
 	});
 }
