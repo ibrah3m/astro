0df24c7ec539bee6ad91657b8c310a4447d5259d
ibrah3m <91458548+ibrah3m@users.noreply.github.com>
2023-01-18 16:46:48 +0300

    Update gitpod-setup.sh

--- .gitpod/gitpod-setup.sh
+++ .gitpod/gitpod-setup.sh
@@ -8,9 +8,10 @@ if [ "${CONTEXT_URL_ITEMS[7]}" = "examples" ]; then
     EXAMPLE_PROJECT=${CONTEXT_URL_ITEMS[8]}
 # Check it Gitpod started with $ASTRO_NEW environment variable
 elif [ -n "$ASTRO_NEW" ]; then
-    $reponame ="ASTRO_NEW"
+    reponame="ASTRO_NEW"
+    echo $reponame
     //clone from git repo and base on  examples 
-    git clone ${$ASTRO_NEW} ${GITPOD_REPO_ROOT}/${reponame}
+    git clone ${ASTRO_NEW} ${GITPOD_REPO_ROOT}/examples/${reponame}
 
     EXAMPLE_PROJECT="$reponame"
 # Otherwise, set the default example project - 'starter'
@@ -27,5 +28,6 @@ echo "Loading example project:" $EXAMPLE_PROJECT
 cd "$GITPOD_REPO_ROOT"/examples/"$EXAMPLE_PROJECT" || exit
 # Open the main page in VSCode
 code src/pages/index.astro
+
 # Start Astro
 pnpm start
